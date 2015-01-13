41ae2dce9a38ba231889766a26f649fdaa479a8b
PilgrimShadow <the.sly.stoat@gmail.com>
2015-01-12 23:07:48 -0600

    perfecting Walker physics

--- build.sbt
+++ build.sbt
@@ -15,7 +15,7 @@ libraryDependencies ++= Seq(
   "org.scala-lang.modules.scalajs" %%% "scalajs-dom" % "0.6"
 )
 
-bootSnippet := "StickMan.main(document.getElementById('canvas'));"
+bootSnippet := "ArcadeShooter.main(document.getElementById('canvas'));"
 
 updateBrowsers <<= updateBrowsers.triggeredBy(ScalaJSKeys.fastOptJS in Compile)
 
--- src/main/scala/example/ArcadeShooter.scala
+++ src/main/scala/example/ArcadeShooter.scala
@@ -1,5 +1,9 @@
 package example
 
+// TODO: Refactor into multiple files
+// TODO: Add method to World class that introduces citizens into world
+// TODO: Add collision detection. (keep separate list of bullets and enemies?)
+
 import scalajs.js
 import org.scalajs.dom
 import scala.scalajs.js.annotation.JSExport
@@ -46,6 +50,7 @@ class Bullet(_x: Int, _y: Int, _vxi: Int, _vyi: Int) extends Citizen {
   var vx = _vxi
   var vy = _vyi
 
+  // change this
   def expired(): Boolean = {
     (x < 0) || (y < 0) || (x > 500) || (y > 500)
   }
@@ -62,26 +67,32 @@ class Bullet(_x: Int, _y: Int, _vxi: Int, _vyi: Int) extends Citizen {
     g.arc(x, y, 5, 0, 2 * math.Pi, anticlockwise = false)
     g.fill()
   }
-
 }
 
 // A fading trail could be implemented using a
 // queue
+
+// A walker is a user-controlled agent that walks across the bottom of the canvas. Used
+// for arcade-style games. (i.e. Galaga)
 class Walker(_pace: Int, _length: Int, _width: Int) extends Citizen {
 
+  // The length of the Walker in pixels.
   val length: Int = _length
+
+  // The width of the walker in pixels.
   val width: Int = _width
 
-  // Pace of the walker
+  // Pace of the walker, that is, the velocity when walking left or right.
   val pace: Int = math.abs(_pace)
 
   // Position
   var x: Int = 250
-  var y: Int = 490
+  var y: Int = 500 - (length / 2)
 
   // Velocity
   var vx: Int = 0
 
+  // Flags indicating the current state of the Walker.
   var movingRight: Boolean = false
   var movingLeft: Boolean = false
   var stopping: Boolean = false
@@ -104,10 +115,28 @@ class Walker(_pace: Int, _length: Int, _width: Int) extends Citizen {
     }
   }
 
+
+  // Subtly wrong from a UI point of view.
   def stopMoving(): Unit = {
     stopping = true
     movingRight = false
     movingLeft = false
+  }
+
+  // Stops moving right, only if the Walker was already moving right.
+  def stopMovingRight(): Unit = {
+    if (movingRight) {
+      movingRight = false
+      stopping = true
+    }
+  }
+
+  // Stops moving left, only if the Walker was already moving left.
+  def stopMovingLeft(): Unit = {
+    if (movingLeft) {
+      movingLeft = false
+      stopping = true
+    }
   }
 
   private def reduceVel(n: Int): Unit = {
@@ -116,10 +145,15 @@ class Walker(_pace: Int, _length: Int, _width: Int) extends Citizen {
 
   // Idea: Use a "future stack" to handle effects that unfold over the coming frames.
   def update(): Unit = {
+
+    // If we're stopping and moving, slowdown.
     if (stopping && math.abs(vx) > 0) reduceVel(1)
+
+    // Update position
     x += vx
   }
 
+  // Walkers do not spawn anything.
   def spawn(): List[Citizen] = Nil
 
   // never expires
@@ -127,12 +161,15 @@ class Walker(_pace: Int, _length: Int, _width: Int) extends Citizen {
 
   def render(g: dom.CanvasRenderingContext2D): Unit = {
     g.beginPath()
-    g.fillRect(x - (width / 2), 500 - length, width, length)
+    g.fillRect(x - (width / 2), y - (length / 2), width, length)
   }
 }
 
+// Might not need a class for this anymore, since all citizens have a spawn method.
+// Instead, have an object that contains a collection of different style shooters.
 class Shooter extends Walker(10, 50, 30) {
 
+  // A flag indicating what the Shooter should spawn(fire) during the next update.
   private var nextShot: Int = 0
 
   def fireSingle(): Unit = {
@@ -148,25 +185,22 @@ class Shooter extends Walker(10, 50, 30) {
   }
 
 
-  // Idea, have the update method return the spawn list...
-  // Nah, keep it modular.
   override def spawn(): List[Citizen] = {
-    nextShot match {
-      case 1 =>
-        nextShot = 0
-        List(new Bullet(x, 500 - length, 0, -10))
 
-      case 2 =>
-        nextShot = 0
-        for (i <- List.range(-2, 4, 2)) yield new Bullet(x, 500 - length, i, -10)
+    // We will match on the current state of the nextShot flag.
+    val toShoot = nextShot
 
-      case 3 =>
-        nextShot = 0
-        for (i <- List.range(0, 21)) yield new Bullet(x, 500, (40 * math.cos((i / 20.0) * math.Pi)).toInt / 3, -(40 * math.sin((i / 20.0) * math.Pi)).toInt / 3)
+    // Always reset for the next update.
+    nextShot = 0
+
+    toShoot match {
+      case 1 => List(new Bullet(x, y - (length / 2), 0, -10))
+
+      case 2 => for (i <- List.range(-2, 4, 2)) yield new Bullet(x, y - (length / 2), i, -10)
+
+      case 3 => for (i <- List.range(0, 21)) yield new Bullet(x, y + (length / 2), (250 * math.cos((i / 20.0) * math.Pi)).toInt / 15, -(250 * math.sin((i / 20.0) * math.Pi)).toInt / 15)
 
       case _ => Nil
-
-
     }
   }
 
@@ -174,22 +208,28 @@ class Shooter extends Walker(10, 50, 30) {
 
 class World(_citizens: List[Citizen]) {
 
+  // A list of all the citizens in the world.
   var citizens: List[Citizen] = _citizens
 
   def updateAndRender(g: dom.CanvasRenderingContext2D): Unit = {
     var result: List[Citizen] = Nil
     for (c <- citizens) {
+
+      // If a citizen is expired, we remove it from the world.
       if (!c.expired()) {
 
+        // Render needs to come before the update.
         c.render(g)
 
         // Update all of the citizens of the world.
-        c.update() // doh! - I forgot this line for just a moment, but careful thought quickly revealed the error.
+        c.update()
 
         // The next generation includes the spawn of this generation.
         result = c :: c.spawn() ::: result
       }
     }
+
+    // Update the citizens of the world.
     citizens = result
   }
 
@@ -211,6 +251,11 @@ object StickMan {
     canvas.style.outline = "none"
 
     val wakka = new Shooter
+
+
+    // Idea: provide the world class with methods for adding citizens. This
+    // will make it easier to inform the citizens of certain world-properties.
+
     val world: World = new World(List(wakka))
 
     def renderWorld(): Unit = {
@@ -233,8 +278,8 @@ object StickMan {
 
     canvas.onkeyup = (e: dom.KeyboardEvent) => e.keyCode match {
       // moving left/right
-      case 37 => wakka.stopMoving()
-      case 39 => wakka.stopMoving()
+      case 37 => wakka.stopMovingLeft()
+      case 39 => wakka.stopMovingRight()
     }
 
 
